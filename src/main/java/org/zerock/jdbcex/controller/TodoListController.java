a07df25161e6e3757e3c26c5576a73deb867a13e
JBKIM <drv98@naver.com>
2024-04-17 16:58:08 +0900

    4월 17일 수업

--- src/main/java/org/zerock/jdbcex/controller/TodoListController.java
+++ src/main/java/org/zerock/jdbcex/controller/TodoListController.java
@@ -1,4 +1,23 @@
 package org.zerock.jdbcex.controller;
 
-public class TodoListController {
+import lombok.extern.log4j.Log4j2;
+import org.zerock.jdbcex.service.TodoService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/todo/list")
+@Log4j2
+public class TodoListController extends HttpServlet {
+
+    private TodoService todoService = TodoService.INSTANCE;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("todo list ...........");
+    }
 }
