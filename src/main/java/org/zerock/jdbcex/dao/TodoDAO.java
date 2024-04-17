137c5a38bc4ad1928a63fac12d159cf3886b5e44
JBKIM <drv98@naver.com>
2024-04-17 10:09:38 +0900

    4월 17일 수업

--- src/main/java/org/zerock/jdbcex/dao/TodoDAO.java
+++ src/main/java/org/zerock/jdbcex/dao/TodoDAO.java
@@ -1,9 +1,9 @@
 package org.zerock.jdbcex.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import lombok.Cleanup;
+import org.zerock.jdbcex.domain.TodoVO;
+
+import java.sql.*;
 
 public class TodoDAO {
 
@@ -28,4 +28,37 @@ public class TodoDAO {
 
         return now;
     }
+
+    public String getTime2() throws Exception {
+
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement =
+                connection.prepareStatement("select now() from dual");
+        @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        return resultSet.getString(1);
+    }
+
+    //사용자가 입력한 할일을 데이터베이스에 저장함
+    public void insert(TodoVO vo) throws Exception {
+        String sql = "insert into tbl_todo (title,dueDate,finished) values (?,?,?)";
+
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        //sql 객체에 ?에 실제 값을 입력함
+        preparedStatement.setString(1, vo.getTitle());
+        preparedStatement.setDate(2, Date.valueOf(vo.getDueDate()));
+        preparedStatement.setBoolean(3, vo.isFinished());
+
+        preparedStatement.executeUpdate(); //sql 을 실행함
+    }
 }
+
+
+
+
+
+
+
+
+
--- src/test/java/org/zerock/dao/TodoDAOTests.java
+++ src/test/java/org/zerock/dao/TodoDAOTests.java
@@ -3,6 +3,9 @@ package org.zerock.dao;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.zerock.jdbcex.dao.TodoDAO;
+import org.zerock.jdbcex.domain.TodoVO;
+
+import java.time.LocalDate;
 
 public class TodoDAOTests {
 
@@ -16,6 +19,17 @@ public class TodoDAOTests {
 
     @Test
     public void testTime() throws Exception {
-        System.out.println(todoDAO.getTime());
+        System.out.println(todoDAO.getTime2());
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+
+        TodoVO todoVO = TodoVO.builder()
+                .title("샘플 타이틀...")
+                .dueDate(LocalDate.of(2024,4,17))
+                .build();
+
+        todoDAO.insert(todoVO);
     }
 }
